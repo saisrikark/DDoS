d15e1034392a966fc008c5d908a52aab2d348711
Sai Srikar K <saisrikark@localhost.localdomain>
2020-02-04 21:06:00 +0530

    Base packet without parameters

--- DDoS/DDoS_helpers/create.py
+++ DDoS/DDoS_helpers/create.py
@@ -4,9 +4,36 @@ most of the simple parameters will be placed here
 to change the attack object
 """
 
-def base_attack_class(parameter_list):
-    def __init__(self):
-        print("In base attack class")
+from scapy.all import IP, TCP, UDP, ICMP, send
+
+class base_attack_class():
+    
+    def __init__(self, script_args):
+        self.packets = []
+        self.script_args = script_args
+        
+    def construct_empty_packets(self):
+        packet_dict = {
+            "IP" : IP(),
+            "TCP" : IP()/TCP(),
+            "UDP" : IP()/UDP(),
+            "ICMP" : IP()/ICMP()
+        }
+        protocol = self.script_args["protocol"]
+        required_base_packet = packet_dict[protocol]
+        #required_base_packet.src = "1.1.1.1"
+        #required_base_packet.dest = "127.0.0.1"
+        self.packets.append(required_base_packet)
+
+    def add_base_packet_parameters(self):
+        # Whatever base parameters are required, will go here
+        """for index in len(self.packets):
+            self.packets[index].src = "1.1.1.1"
+            self.packets[index].dest = "127.0.0.1"
+        """
 
 def create_ddos_attack_object(script_args):
-    return base_attack_class(script_args)
+    base_attack_object = base_attack_class(script_args)
+    base_attack_object.construct_empty_packets()
+    base_attack_object.add_base_packet_parameters()
+    return base_attack_object
--- DDoS/attack.py
+++ DDoS/attack.py
@@ -11,22 +11,21 @@ import sys
 from dependencies import *
 
 def retrieve_arguments():
-    # Register all the arguments from the yaml file
-    ap = argparse.ArgumentParser(description="Parsing arguments")
+    # Retrieve all the arguments from the yaml file
     f = open(YAML_FILE, "r")
     yaml_data = yaml.load(f, Loader=yaml.FullLoader)
+    args = {}
     for arg in yaml_data.keys():
-        ap.add_argument(arg, help=yaml_data[arg]["help"])
-    args = ap.parse_args()
+        args[yaml_data[arg]["name"]] = yaml_data[arg]["value"]
     return args
 
 def perform_attack():
     script_args = retrieve_arguments()
     ddos_attack_object = create_ddos_attack_object(script_args)
-    modified_ddos_attack_object = modify_ddos_attack_object(
-        ddos_attack_object, script_args)
-    send_ddos_attack_object(modified_ddos_attack_object,
-     script_args)
+    #modified_ddos_attack_object = modify_ddos_attack_object(
+    #    ddos_attack_object, script_args)
+    #send_ddos_attack_object(modified_ddos_attack_object,
+    # script_args)
 
 if __name__ == "__main__":
     perform_attack()

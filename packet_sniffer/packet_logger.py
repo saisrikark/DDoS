99046a750b2c6183aa5f1c5ec58aa2aa7ef4f96f
ShashankBongale <shashankbongale@gmail.com>
2020-02-21 17:00:07 +0530

    added fieldnames

--- packet_sniffer/packet_logger.py
+++ packet_sniffer/packet_logger.py
@@ -6,13 +6,13 @@ no_of_packets = len(capture)
 fp = open("packets.log", "a")
 for i in range(no_of_packets):
     try:
-        packet_log = str(i) + " " + str(capture[i].sniff_timestamp) + " "
-        packet_log = packet_log + capture[i]['eth'].src + " " + capture[i]['eth'].dst + " " 
-        packet_log = packet_log + capture[i][capture[i].transport_layer.lower()].srcport + " " + capture[i][capture[i].transport_layer.lower()].dstport + " "
+        packet_log = str(i) + " " + "timestamp=" + str(capture[i].sniff_timestamp) + " "
+        packet_log = packet_log + "src_mac_addr=" +  capture[i]['eth'].src + " " + "dst_mac_addr=" + capture[i]['eth'].dst + " " 
+        packet_log = packet_log + "src_port=" + capture[i][capture[i].transport_layer.lower()].srcport + " " + "dst_port=" + capture[i][capture[i].transport_layer.lower()].dstport + " "
         if("ip" in capture[i]):
-            packet_log = packet_log + capture[i]['ip'].src + " " + capture[i]['ip'].dst + " "
+            packet_log = packet_log + "src_ip=" + capture[i]['ip'].src + " " + "dst_ip=" + capture[i]['ip'].dst + " "
         else:
-            packet_log = packet_log + capture[i]['ipv6'].src + " " + capture[i]['ipv6'].dst + "\n"
+            packet_log = packet_log + "src_ip=" + capture[i]['ipv6'].src + " " +"dst_ip=" +  capture[i]['ipv6'].dst + "\t\n"
         fp.write(packet_log)
     except:
         pass

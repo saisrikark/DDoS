d12af0c83a3f8b1bc7b491344db9fa587abb46c1
ShashankBongale <shashankbongale@gmail.com>
2020-02-21 17:33:16 +0530

    real time monitoring

--- packet_sniffer/packet_logger.py
+++ packet_sniffer/packet_logger.py
@@ -1,18 +1,20 @@
 import pyshark
 
 capture = pyshark.LiveCapture(interface = 'wlp19s0')
-capture.sniff(timeout = 200)
+capture.sniff(timeout = 10)
 no_of_packets = len(capture)
-for i in range(no_of_packets):
+j = 0
+for i in capture:
     try:
         fp = open("packets.log", "a")
-        packet_log = str(i) + " " + str(capture[i].sniff_timestamp) + " "
-        packet_log = packet_log + capture[i]['eth'].src + " " + capture[i]['eth'].dst + " " 
-        packet_log = packet_log + capture[i][capture[i].transport_layer.lower()].srcport + " " + capture[i][capture[i].transport_layer.lower()].dstport + " "
-        if("ip" in capture[i]):
-            packet_log = packet_log + capture[i]['ip'].src + " " + capture[i]['ip'].dst + "\n"
+        packet_log = str(j) + " " + str(i.sniff_timestamp) + " "
+        j = j + 1
+        packet_log = packet_log + i['eth'].src + " " + i['eth'].dst + " " 
+        packet_log = packet_log + i[i.transport_layer.lower()].srcport + " " + i[i.transport_layer.lower()].dstport + " "
+        if("ip" in i):
+            packet_log = packet_log + i['ip'].src + " " + i['ip'].dst + "\n"
         else:
-            packet_log = packet_log + capture[i]['ipv6'].src + " " + capture[i]['ipv6'].dst + "\n"
+            packet_log = packet_log + i['ipv6'].src + " " + i['ipv6'].dst + "\n"
         fp.write(packet_log)
         fp.close()
     except:
